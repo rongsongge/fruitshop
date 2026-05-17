9ab69c1ea097065e8884e0fc7ec82614991dc1d4
rongsongge <3045142985@qq.com>
2026-05-17 19:19:36 +0800

    Fix image file extensions in product list
    Updated image file extensions from .jpg to .JPG.

--- product.js
+++ product.js
@@ -4,7 +4,7 @@ const rawProductList = [
     name: "红富士苹果",
     price: 8,
     stock: 100,
-    img: "01.jpg",
+    img: "01.JPG",
     desc: "脆甜多汁，新鲜采摘"
   },
   {
@@ -12,7 +12,7 @@ const rawProductList = [
     name: "进口车厘子",
     price: 39.9,
     stock: 50,
-    img: "02.jpg",
+    img: "02.JPG",
     desc: "J级大果，颗颗饱满"
   },
   {
@@ -20,7 +20,7 @@ const rawProductList = [
     name: "阳光玫瑰葡萄",
     price: 25.8,
     stock: 30,
-    img: "03.jpg",
+    img: "03.JPG",
     desc: "晴王品种，无籽香甜"
   }
 ];
